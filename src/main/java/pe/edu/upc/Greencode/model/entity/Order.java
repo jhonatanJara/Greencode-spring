7adffadcb7ff562684ef05071093a04456c96ba1
Kevin <kcvj_2295@hotmail.com>
2021-05-30 23:23:35 -0500

    ORDER Y TRANSACTION

---
+++ src/main/java/pe/edu/upc/Greencode/model/entity/Order.java
@@ -0,0 +1,150 @@
+package pe.edu.upc.Greencode.model.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+
+@Entity
+@Table(name="Transactions")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name = "order_id", nullable = false)
+	private int id;
+	
+	@Column(name="date", nullable=false)
+	@Temporal(TemporalType.DATE)
+	private Date date;
+	
+	@Column(name="total_amount", nullable=true, columnDefinition = "DECIMAL(5,2)")
+	private Float totalAmount;
+	
+	@Column(name="arrival_time", nullable=false)
+	private Integer arrivalTime;
+	
+	@Column(name="status", nullable = false, length = 20)
+	private String status;
+
+	@ManyToOne
+	@JoinColumn(name="recycler_id", nullable = false)
+	private Recycler recycler;
+
+	public Order() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Float getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(Float totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public Integer getArrivalTime() {
+		return arrivalTime;
+	}
+
+	public void setArrivalTime(Integer arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Recycler getRecycler() {
+		return recycler;
+	}
+
+	public void setRecycler(Recycler recycler) {
+		this.recycler = recycler;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((arrivalTime == null) ? 0 : arrivalTime.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((recycler == null) ? 0 : recycler.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((totalAmount == null) ? 0 : totalAmount.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (arrivalTime == null) {
+			if (other.arrivalTime != null)
+				return false;
+		} else if (!arrivalTime.equals(other.arrivalTime))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (id != other.id)
+			return false;
+		if (recycler == null) {
+			if (other.recycler != null)
+				return false;
+		} else if (!recycler.equals(other.recycler))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (totalAmount == null) {
+			if (other.totalAmount != null)
+				return false;
+		} else if (!totalAmount.equals(other.totalAmount))
+			return false;
+		return true;
+	}
+	
+	
+}
---
+++ src/main/java/pe/edu/upc/Greencode/model/entity/Transaction.java
@@ -0,0 +1,174 @@
+package pe.edu.upc.Greencode.model.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+
+
+@Entity
+@Table(name="Transactions")
+//indexes = { @Index(columnList = "first_name", name = "recyclers_index_first_name")})
+
+public class Transaction {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name = "transaction_id", nullable = false)
+	private int id;
+	
+	@Column(name="name", nullable=false, length=20) 
+	private String name;
+	
+	@Column(name="date", nullable=false)
+	@Temporal(TemporalType.DATE)
+	private Date date;
+	
+	@Column(name="type_payment", nullable=false, length=20)
+	private String typePayment;
+	
+	@Column(name="type_transaction", nullable=false, length=20)
+	private String typeTransaction;
+	
+	@Column(name="amount", nullable=false, columnDefinition = "DECIMAL(3,1)")
+	private Float amount;
+	
+	//COMENTARIO
+	//@ManyToOne
+	//@JoinColumn(name = "account_id", nullable = false)
+	//private Account account;
+
+	public Transaction() {
+		super();
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((typePayment == null) ? 0 : typePayment.hashCode());
+		result = prime * result + ((typeTransaction == null) ? 0 : typeTransaction.hashCode());
+		return result;
+	}
+
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getTypePayment() {
+		return typePayment;
+	}
+
+	public void setTypePayment(String typePayment) {
+		this.typePayment = typePayment;
+	}
+
+	public String getTypeTransaction() {
+		return typeTransaction;
+	}
+
+	public void setTypeTransaction(String typeTransaction) {
+		this.typeTransaction = typeTransaction;
+	}
+
+	public Float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Float amount) {
+		this.amount = amount;
+	}
+
+	/*
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	 */
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transaction other = (Transaction) obj;
+		if (amount == null) {
+			if (other.amount != null)
+				return false;
+		} else if (!amount.equals(other.amount))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (typePayment == null) {
+			if (other.typePayment != null)
+				return false;
+		} else if (!typePayment.equals(other.typePayment))
+			return false;
+		if (typeTransaction == null) {
+			if (other.typeTransaction != null)
+				return false;
+		} else if (!typeTransaction.equals(other.typeTransaction))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
