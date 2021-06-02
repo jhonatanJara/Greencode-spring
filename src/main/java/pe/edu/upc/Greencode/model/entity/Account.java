cc2059f1ec021941d6ec8e8363c38ea8666b420a
Brignith Izumi Gomez Vargas <brignith.2@gmail.com>
2021-06-01 22:26:44 -0500

    NEW ACCOUNT

--- src/main/java/pe/edu/upc/Greencode/model/entity/Account.java
+++ src/main/java/pe/edu/upc/Greencode/model/entity/Account.java
@@ -1,14 +1,18 @@
 package pe.edu.upc.Greencode.model.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -32,11 +36,23 @@ public class Account {
 	@ManyToOne
 	@JoinColumn(name="gatherer_id", nullable = false)
 	private Gatherer gatherer;
+	
+	@OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
+	private List<Transaction> Transaction;
 
 	public Account() {
 		super();
+		Transaction = new  ArrayList<Transaction>();
 	}
 
+	public List<Transaction> getTransaction() {
+		return Transaction;
+	}
+
+	public void setTransaction(List<Transaction> Transaction) {
+		this.Transaction = Transaction;
+	}
+	
 	public int getId() {
 		return id;
 	}
