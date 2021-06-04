0f791f5d2e0543781ca60a8a142fc01d85009f4f
Jhonatan <jara_984288901@hotmail.com>
2021-06-04 14:34:33 -0500

    Dashboard

--- src/main/java/pe/edu/upc/Greencode/controller/GathererController.java
+++ src/main/java/pe/edu/upc/Greencode/controller/GathererController.java
@@ -1,5 +1,13 @@
 package pe.edu.upc.Greencode.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class GathererController {
-//aea
+	
+	@GetMapping("/dashboard")
+	public String Home() {
+		return "dashboard";
+	}
 }
