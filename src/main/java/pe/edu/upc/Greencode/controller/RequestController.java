d8a5c3baa1855b8cdba9f93a176c1454dbbe9e86
GerNic <geraldoparionareyes@hotmail.com>
2021-06-15 17:14:17 -0500

    Request:fase 4

--- src/main/java/pe/edu/upc/Greencode/controller/RequestController.java
+++ src/main/java/pe/edu/upc/Greencode/controller/RequestController.java
@@ -1,5 +1,6 @@
 package pe.edu.upc.Greencode.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,15 +11,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pe.edu.upc.Greencode.model.entity.Order;
 import pe.edu.upc.Greencode.model.entity.Recycler;
+import pe.edu.upc.Greencode.model.entity.Waste;
 import pe.edu.upc.Greencode.service.RecyclerService;
+import pe.edu.upc.Greencode.service.WasteService;
 
 @Controller
 @RequestMapping("/request")
 public class RequestController {
-
+	
 	@Autowired
 	private RecyclerService recyclerService;
+	
+	@Autowired
+	private WasteService wasteService;
+	
 	
 	@GetMapping
 	public String list(Model model) {
@@ -35,15 +43,22 @@ public class RequestController {
 	@GetMapping("{id}")
 	public String findById(Model model,@PathVariable("id") Integer id) {
 		try {
-			Optional<Recycler> optional=recyclerService.findById(id);
-			if(optional.isPresent()) {
-				model.addAttribute("recycler", optional.get());//Extrae el elemeto del optional y lo envia como atributo
-				return "request/view";
+			Optional<Recycler> recycler = recyclerService.findById(id);
+			List<Waste> waste= wasteService.getAll();
+			List<Waste> waste1 = new ArrayList<Waste>();
+			for(int i=0; i< waste.size(); i++) {
+				if(waste.get(i).getRecycler().getId()==recycler.get().getId()) {
+					waste1.add(waste.get(i));
+				}
 			}
+				model.addAttribute("waste1", waste1);
+				return "request/view";
 		}catch(Exception e){
 			e.printStackTrace();
 			System.err.println(e.getMessage());
 		}
 		return "redirect:/request";
 	}
+	
+	
 }
