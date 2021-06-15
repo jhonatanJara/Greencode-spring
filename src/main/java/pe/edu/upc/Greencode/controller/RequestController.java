cc484969746eaf4343a7d6ddcacaf9d9eebaed8c
GerNic <geraldoparionareyes@hotmail.com>
2021-06-15 01:32:10 -0500

    Request:Fase 2

--- src/main/java/pe/edu/upc/Greencode/controller/RequestController.java
+++ src/main/java/pe/edu/upc/Greencode/controller/RequestController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pe.edu.upc.Greencode.model.entity.District;
 import pe.edu.upc.Greencode.model.entity.Recycler;
+import pe.edu.upc.Greencode.service.DistrictService;
 import pe.edu.upc.Greencode.service.RecyclerService;
 
 @Controller
