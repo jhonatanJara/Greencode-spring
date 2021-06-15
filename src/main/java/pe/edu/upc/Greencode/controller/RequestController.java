98666f3b188fd8ccf0aab411da1d26083159a644
GerNic <geraldoparionareyes@hotmail.com>
2021-06-15 11:27:41 -0500

    Request: fase 3

--- src/main/java/pe/edu/upc/Greencode/controller/RequestController.java
+++ src/main/java/pe/edu/upc/Greencode/controller/RequestController.java
@@ -1,16 +1,16 @@
 package pe.edu.upc.Greencode.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import pe.edu.upc.Greencode.model.entity.District;
 import pe.edu.upc.Greencode.model.entity.Recycler;
-import pe.edu.upc.Greencode.service.DistrictService;
 import pe.edu.upc.Greencode.service.RecyclerService;
 
 @Controller
@@ -31,6 +31,19 @@ public class RequestController {
 		}
 		return "request/list";
 	}
-	
-
+		
+	@GetMapping("{id}")
+	public String findById(Model model,@PathVariable("id") Integer id) {
+		try {
+			Optional<Recycler> optional=recyclerService.findById(id);
+			if(optional.isPresent()) {
+				model.addAttribute("recycler", optional.get());//Extrae el elemeto del optional y lo envia como atributo
+				return "request/view";
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		return "redirect:/request";
+	}
 }
