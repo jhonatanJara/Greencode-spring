7a9efc41b1cdb81214c425e50f6a7ed59892af0c
lilianahuarocc <silviana.18.987@gmail.com>
2021-06-15 16:07:26 -0500

    More Details Purchase History

--- src/main/java/pe/edu/upc/Greencode/controller/HistoryController.java
+++ src/main/java/pe/edu/upc/Greencode/controller/HistoryController.java
@@ -1,4 +1,5 @@
 package pe.edu.upc.Greencode.controller;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import pe.edu.upc.Greencode.model.entity.Coupon;
 import pe.edu.upc.Greencode.model.entity.Order;
 import pe.edu.upc.Greencode.model.entity.Recycler;
+import pe.edu.upc.Greencode.model.entity.Waste;
 import pe.edu.upc.Greencode.service.OrderService;
 import pe.edu.upc.Greencode.service.RecyclerService;
+import pe.edu.upc.Greencode.service.WasteService;
 
 @Controller
 @RequestMapping("/history")
@@ -23,6 +26,9 @@ public class HistoryController {
 	
 	@Autowired
 	private RecyclerService recyclerService;
+	
+	@Autowired
+	private WasteService wasteService;
 	
 	@GetMapping("purchase")
 	public String listHistoryPurchase(Model model) {
@@ -40,10 +46,16 @@ public class HistoryController {
 	public String findHistoryPurchaseById(Model model, @PathVariable("id") Integer id) {
 		try {
 			Optional<Order> optional = orderSevice.findById(id);
-			if (optional.isPresent()) {
-				model.addAttribute("order", optional.get());
-				return "history/viewPurchase";
-			}	
+			Optional<Recycler> recycler= recyclerService.findById(optional.get().getRecycler().getId());
+			List<Waste> waste= wasteService.getAll();
+			List<Waste> waste1 = new ArrayList<Waste>();
+			for(int i=0; i< waste.size(); i++) {
+				if(waste.get(i).getRecycler().getId()==recycler.get().getId()) {
+					waste1.add(waste.get(i));
+				}
+			}
+				model.addAttribute("waste1", waste1);
+				return "history/viewPurchase";	
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
