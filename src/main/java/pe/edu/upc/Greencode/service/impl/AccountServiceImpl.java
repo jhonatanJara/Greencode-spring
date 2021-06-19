2b1e12411b0b68e4971280c5d65d5fc705c14e5f
Brignith Izumi Gomez Vargas <brignith.2@gmail.com>
2021-06-19 14:49:50 -0500

    NUEVO CAMBIO

--- src/main/java/pe/edu/upc/Greencode/service/impl/AccountServiceImpl.java
+++ src/main/java/pe/edu/upc/Greencode/service/impl/AccountServiceImpl.java
@@ -2,11 +2,13 @@ package pe.edu.upc.Greencode.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import pe.edu.upc.Greencode.model.entity.Account;
 import pe.edu.upc.Greencode.model.repository.AccountRepository;
 import pe.edu.upc.Greencode.service.AccountService;
 
+@Service
 public class AccountServiceImpl implements AccountService{
 	@Autowired
 	private AccountRepository accountRepository;
