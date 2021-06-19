aea734d90949d0593985c0d2213aaeba1ae5d7be
Kevin <kcvj_2295@hotmail.com>
2021-06-19 15:54:15 -0500

    Sales details and status

--- src/main/java/pe/edu/upc/Greencode/controller/HistoryController.java
+++ src/main/java/pe/edu/upc/Greencode/controller/HistoryController.java
@@ -10,9 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.edu.upc.Greencode.model.entity.Gatherer;
 import pe.edu.upc.Greencode.model.entity.Order;
 import pe.edu.upc.Greencode.model.entity.Recycler;
 import pe.edu.upc.Greencode.model.entity.Waste;
+import pe.edu.upc.Greencode.service.GathererService;
 import pe.edu.upc.Greencode.service.OrderService;
 import pe.edu.upc.Greencode.service.RecyclerService;
 import pe.edu.upc.Greencode.service.WasteService;
@@ -28,6 +31,9 @@ public class HistoryController {
 	
 	@Autowired
 	private RecyclerService recyclerService;
+	
+	@Autowired
+	private GathererService gathererService;
 	
 	@Autowired
 	private WasteService wasteService;
@@ -82,13 +88,8 @@ public class HistoryController {
 		try {
 			Optional<Order> optional = orderSevice.findById(id);
 			Optional<Recycler> recycler= recyclerService.findById(optional.get().getRecycler().getId());
-			List<Waste> waste= wasteService.getAll();
-			List<Waste> waste1 = new ArrayList<Waste>();
-			for(int i=0; i< waste.size(); i++) {
-				if(waste.get(i).getRecycler().getId()==recycler.get().getId()) {
-					waste1.add(waste.get(i));
-				}
-			}
+			List<Waste> waste1= recycler.get().getWastes();
+			
 				model.addAttribute("optional", optional.get());
 				model.addAttribute("waste1", waste1);
 				model.addAttribute("wasteSearch", wasteSearch);
