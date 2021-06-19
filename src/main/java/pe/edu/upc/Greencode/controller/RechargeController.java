260fcc4041348669e11fce23db0e1938b8a9cb62
Brignith Izumi Gomez Vargas <brignith.2@gmail.com>
2021-06-19 14:43:02 -0500

    RECHARGE

--- src/main/java/pe/edu/upc/Greencode/controller/RechargeController.java
+++ src/main/java/pe/edu/upc/Greencode/controller/RechargeController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
-//nuevo
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
