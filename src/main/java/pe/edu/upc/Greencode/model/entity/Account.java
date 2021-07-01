60e2edf90efdc828e447529fa70793ee092aed8e
HP <HP@192.168.1.6>
2021-07-01 12:00:27 -0500

    new change

--- src/main/java/pe/edu/upc/Greencode/model/entity/Account.java
+++ src/main/java/pe/edu/upc/Greencode/model/entity/Account.java
@@ -17,7 +17,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name="Accounts")
 public class Account {
- 
+  //new account
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	@Column(name = "account_id", nullable = false)
